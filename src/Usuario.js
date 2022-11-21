d0a565ac9960dbe27282e84f2c17dfe6123cde4a
Matheus <matheusvmja@gmail.com>
2022-11-21 16:18:21 -0300

    trocar nome e imagem

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,14 +1,29 @@
+import React from 'react'
+
 export default function Usuario() {
+    const [nome, setNome] = React.useState("Catana")
+    const [imagem, setImagem] = React.useState("assets/img/catanacomics.svg")
+
+    function trocarNome() {
+        const novoNome = prompt("Escolha um novo nome")
+        setNome(novoNome)
+    }
+
+    function trocarImagem() {
+        const novaImagem = prompt("Escolha uma nova imagem")
+        setImagem(novaImagem)
+    }
+
     return (
         <div class="usuario">
-            <img src="assets/img/catanacomics.svg" />
+            <img onClick={trocarImagem} src={imagem} />
             <div class="texto">
                 <strong>catanacomics</strong>
-                <span>
-                    Catana
-                    <ion-icon name="pencil"></ion-icon>
+                <span onClick={trocarNome}>
+                    {nome}
+                    <ion-icon name="pencil" ></ion-icon>
                 </span>
             </div>
         </div>
     )
-}
+} 
